cea761d371088604db297e014ce5ef6d7bd85d58
MKWong <107278431+maverickwong17@users.noreply.github.com>
2022-08-21 13:29:31 -0700

    get db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -3,19 +3,31 @@ import { openDB } from 'idb';
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
-        return;
-      }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+		if (db.objectStoreNames.contains('jate')) {
+			console.log('jate database already exists');
+			return;
+		}
+		db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+		console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+	console.log('putDb')
+	
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+    console.log('getDb');
+	const jateDB = await openDB('jate',1);
+	const action = jateDB.transaction('jate', 'readonly')
+	const storeData = action.objectStore('jate')
+	const get = storeData.getAll()
+	const output = await get;
+	console.log('output.value', output)
+	return output
+};
 
 initdb();
